2af4aa9b1584130ba8bf4b14ae064c63e08561fd
mattcbodily <mattcbodily@gmail.com>
2021-01-21 09:18:13 -0700

    boiler-plate reset

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import Hooks from './Components/Hooks';
-import ThemeChanger from './Components/ThemeChanger';
-import RenderProp from './Components/RenderProp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Hooks /> */}
+      <Hooks />
       {/* <ThemeChanger /> */}
       {/* render prop component here */}
-      <RenderProp render={(theme, toggleFn) => (
-        <div className={theme}>
-          <button className='toggle-btn' onClick={toggleFn}>{theme}</button>
-        </div>
-      )}/>
     </div>
   );
 }
--- src/Components/Hooks.js
+++ src/Components/Hooks.js
@@ -1,26 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, { Component } from 'react'
 
-export default props => {
-    let [username, setUsername] = useState(''),
-        [count, setCount] = useState(0);
+class Hooks extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            username: '',
+            count: 0
+        }
+    }
 
-    //Simulates componentDidMount
-    useEffect(() => {
-        //axios request or way of getting here
-        setUsername('Matt')
-    }, [])
+    componentDidMount(){
+        this.setState({ username: 'Matt' })
+    }
 
-    //Simulates componentDidUpdate
-    useEffect(() => {
+    componentDidUpdate(){
         console.log('Count changed')
-    }, [count])
+    }
 
-    return (
-        <main>
-            <h1>Hello, {username}, your count is {count}</h1>
-            <input value={username} onChange={e => setUsername(e.target.value)}/>
-            <button onClick={() => setCount(count - 1)}>Count Down</button>
-            <button onClick={() => setCount(count + 1)}>Count Up</button>
-        </main>
-    )
+    handleInput = (val) => {
+        this.setState({ username: val })
+    }
+
+    incrementCount = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
+    decrementCount = () => {
+        this.setState({ count: this.state.count - 1 })
+    }
+
+    render(){
+        return (
+            <main>
+                <h1>Hello, {this.state.username}, your count is {this.state.count}</h1>
+                <input value={this.state.username} onChange={e => this.handleInput(e.target.value)}/>
+                <button onClick={this.decrementCount}>Count Down</button>
+                <button onClick={this.incrementCount}>Count Up</button>
+            </main>
+        )
+    }
 }
+
+export default Hooks;
